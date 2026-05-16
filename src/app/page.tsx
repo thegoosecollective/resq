b9b5198d3c4d54a17eee13df8e62e817a0c97e09
thegoosecollective <honk@thegoosecollective.com>
2026-05-15 21:13:23 -0700

    displaying component and componenet logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import BuildingCodeForm from '@/components/BuildingCodeForm'
+
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <h2>Enter access code:</h2>
-
+<BuildingCodeForm></BuildingCodeForm>
     </div>
   );
 }
