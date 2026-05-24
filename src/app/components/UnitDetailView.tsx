547c93c394439f28aaaf11089eba46da21470b69
thegoosecollective <honk@thegoosecollective.com>
2026-05-23 17:02:25 -0700

    structure of individual unit and role base set up

--- src/app/components/UnitDetailView.tsx
+++ src/app/components/UnitDetailView.tsx
@@ -1,4 +1,7 @@
 'use client'
+import { useState } from 'react'
+import { ResponderStatus } from '@prisma/client'
+import { getResourceLabel } from '@/lib/reportUtils'
 
 type Report = {
     residentStatus: string
@@ -22,5 +25,37 @@ export default function UnitDetailView({
     report: Report
     isResponder: boolean
   }) {
+    const [responderStatus, setResponderStatus] = useState<ResponderStatus | null>(
+        report.responderStatus as ResponderStatus | null
+      )
+      return(
+        <div>
+            <h2>Unit {report.unit.unitNumber}</h2>   
 
-  }
+    <p>Submitted at : {new Date(report.submittedAt).toLocaleString()}</p>
+    <p>Last updated: {new Date(report.updatedAt).toLocaleString()}</p>
+
+    <p>{report.residentStatus}</p>
+<p>Evacuated: {report.occupantsEvacuated}/{report.totalOccupants} occupants</p>
+
+   <p>Resource requests:</p>
+   <ul>
+   {report.resourceRequests.map(r => (
+  <li key={r}>{getResourceLabel(r)}</li>
+))}
+
+   </ul>
+
+   {report.notes &&  isResponder &&(
+  <>
+    <p>Notes:</p>
+    <div>{report.notes}</div>
+    <div>
+    <p>Responder status:</p>
+
+  </div>
+  </>
+)}
+        </div>
+      )
+}
