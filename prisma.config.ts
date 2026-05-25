c06a30451e0e76cf7f6a8ae6f0ae2d62fc5db0de
thegoosecollective <honk@thegoosecollective.com>
2026-05-24 18:27:13 -0700

    fixing prisma config issues for docker

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,8 @@
 import path from 'path'
-import type { PrismaConfig } from 'prisma'
 
 export default {
   schema: path.join('prisma', 'schema.prisma'),
   datasource: {
     url: process.env.DATABASE_URL as string,
   },
-} satisfies PrismaConfig
+}
